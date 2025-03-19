52e19868b03d45e5d44077fcc321d6f4db21e7e3
Davi-Sampaioestec <dixxgabriellasam@gmail.com>
2025-03-19 12:17:08 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+string palavra;
+string letra;
+
+
+Console.WriteLine("Bem vindo ao jogo da forca");
+Console.Write("Digite sua palavra: ");
+palavra = Console.ReadLine()!;
+
+Console.Write("Letra: ");
+letra = Console.ReadLine()!.ToUpper().Trim().Substring(0, 1);
+
+bool sim = palavra.Contains(letra);
+
+Console.WriteLine($"A letra {letra} existe na palavra? - {sim}"); //pq não retorna o resultado certo???
+
+
+
+
+/*char[] palavraFinal = palavra.ToCharArray(); //usar "ToCharArray" somente com a var "char" que armazena um caractere, cabe emoji
+
+foreach (char caractere in palavraFinal) //Foreach - le cada caractere individualmente e altera
+{
+    Console.WriteLine(caractere);
+}*/
